c7a94e4e172e57b68827b86ed7200302adfbc400
guoke <38196962@qq.com>
2015-07-27 08:54:03 +0800

    1、增加TCP优化部分 2、增加修改ssh端口，禁止密码登录，增加ssh key功能 3、

--- d7manyuser.sh
+++ d7manyuser.sh
@@ -6,9 +6,11 @@ if [[ $(id -u) != "0" ]]; then
 fi
 #功能1
 function installEnvironment {
+	#解决public key的问题
+	apt-get install debian-keyring debian-archive-keyring
+	apt-key update
 	apt-get update -y
-	#安装ssh登录保护
-	apt-get install denyhosts 
+
 	#apt-get install unzip -y
 	#修改系统参数限制
 	echo "*                soft    nofile          8192" >>  /etc/security/limits.conf
@@ -31,6 +33,46 @@ function installEnvironment {
 	echo "00 05 * * * root /sbin/reboot" >>/etc/crontab
 	#重启定时器
 	/etc/init.d/cron restart
+	
+	#以下内容是按照教程优化的https://github.com/shadowsocks/shadowsocks/wiki/%E4%BC%98%E5%8C%96-Shadowsocks
+	cat << _EOF_ >/etc/sysctl.d/local.conf
+fs.file-max = 51200
+
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.rmem_default = 65536
+net.core.wmem_default = 65536
+net.core.netdev_max_backlog = 4096
+net.core.somaxconn = 4096
+
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 0
+net.ipv4.tcp_fin_timeout = 30
+net.ipv4.tcp_keepalive_time = 1200
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 4096
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_rmem = 4096 87380 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_congestion_control = hybla
+_EOF_
+#使参数生效
+sysctl --system
+#安装ssh登录保护
+apt-get install denyhosts 
+#更改ssh端口
+ sed -i 's/Port 22/Port 8799/g'  /etc/ssh/sshd_config
+#启用ssh key登录，
+  mkdir /root/.ssh
+ echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiH6uaXQA0K54of5TdpGP9v2z9CaXqnnwyNoCMwkRYPH3X7CJk61HunR59zgDoAbT/+eJiCbSEN5/27gGd1kysct/4PDzJ/JpJI3OhKk2185LRoa/BXZaGfEUz4r53M01tGTMyom9VGjFroTUHeBkj2BfAzIo+SFp+ij1RRzM75ZN/y84rGUvGR8+tM3+PbE+6W0mvY6EdKD0YY0bGtcO9xMaFB7sfZk/fUxQSNnrkfYNKA5rWIlLC3JNXwp0M77dK2pSvU3mGodC6UvWv2GU4Q++tj577M9QTv2bI6Lt7nwQHORcAZ5oknNjTKfO8cyvzN8jjCUKzj5zLave6YJB2Q== rsa-key-20150202" >> /root/.ssh/authorized_keys
+	
+	 sed -i 's/#AuthorizedKeysFile/AuthorizedKeysFile/g' /etc/ssh/sshd_config
+	#禁用密码登录
+	 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+	
 	doselect
  }
 #功能5
@@ -67,7 +109,7 @@ function installmanyuser {
      echo "password = 111111" >> $mypath
 
 	#修改数据库地址
-	 sed -i 's/tuanDB/tuanss'$tuannum'/g' /root/tuanss/Config.py
+	 sed -i 's/tuanDB/tuan'$tuannum'/g' /root/tuanss/Config.py
 	 #将后台管理管理程序的端口随机化
 	 pool=(23330 23331 23332 23333 23334 23335 23336 23337 23338 23339 23339)
 	 num=${#pool[*]}
@@ -103,7 +145,7 @@ function installsspanel {
 	git clone -b new https://doboo@github.com/Doboo/ss-panel.git 
 	mv ss-panel www
 	#修改数据库连接
-	sed -i 's/tuanDB/tuanss'$tuannum'/g' /var/www/lib/config.php
+	sed -i 's/tuanDB/tuan'$tuannum'/g' /var/www/lib/config.php
 	#安装各种依赖，mailgun
 	cd /var/www/
 	curl -sS https://getcomposer.org/installer | php
@@ -141,7 +183,7 @@ function installserverspeeder {
 function doselect {
 echo "Please select your operation "
 echo "which do you want to?input the number."
-echo "1. update system"
+echo "1. systemSet"
 echo "2. install apahe and php"
 echo "3. install ss-panel"
 echo "4. install manyuser"
